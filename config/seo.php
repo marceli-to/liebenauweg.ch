f74e934b78cd68287919d5c58c1c4086b8e736e6
Marcel Stadelmann <m@marceli.to>
2024-08-12 16:05:01 +0200

    wip

---
+++ config/seo.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Seo title (General)
+  |--------------------------------------------------------------------------
+  |
+  | Used for SEO (Pagetitle, Open Graph Title etc.)
+  |
+  */
+
+  'title' => 'Liebenauweg Luzern – Ihr Wohntraum im neuen Glanz!',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Seo description (General)
+  |--------------------------------------------------------------------------
+  |
+  | Used for SEO (Meta description, Open Graph Description)
+  |
+  */
+
+  'description' => 'Bis zum Sommer 2025 entsteht in zentraler Lage im Liebenauweg 6/8 Luzern ein neues Umbauprojekt. Die stilvollen 2.5- bis 4.5-Zimmerwohnungen eignen sich perfekt für Paare als auch Familien.',   
+
+];
