f622221291643e2c58d2ad4b2d4495215b1eb028
Marcel Stadelmann <m@marceli.to>
2024-10-08 11:34:34 +0200

    wip

--- resources/views/pages/imprint.blade.php
+++ resources/views/pages/imprint.blade.php
@@ -4,16 +4,10 @@
   <x-layout.inner class="imprint">
     <h1>Impressum</h1>
     <p>
-      Apleona Schweiz AG<br>
-      Industriestrasse 21<br>
-      8304 Wallisellen<br>
-      <a 
-        href="mailto:mieten@apleona.com" 
-        target="_blank" 
-        rel="noopener" 
-        class="hover:underline underline-offset-2 decoration-1">
-        mieten@apleona.com
-      </a>
+      smeyers Immobilien AG<br>
+      Seetalstrasse 185<br>
+      6032 Emmen<br>
+      <a href="mailto:info@smeyers.ch" target="_blank" class="hover:underline underline-offset-2 decoration-1">info@smeyers.ch</a>
     </p>
     <h2>Design und Entwicklung</h2>
     <p>Stoz Werbeagentur AG<br>Barzloostrasse 2<br>8330 Pfäffikon ZH<br><a href="mailto:hello@stoz.ch" target="_blank" class="hover:underline underline-offset-2 decoration-1">hello@stoz.ch</a><br><a href="https://www.stoz.ch" target="_blank" rel="noopener" class="hover:underline underline-offset-2 decoration-1">www.stoz.ch</a></p>
